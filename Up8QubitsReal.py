1d6ce59a1448263e4df53810bd20e3f6b6512c54
Edvin Martinson <edvmar@student.chalmers.se>
2022-02-26 03:17:27 +0100

    Införde en del funktioner, för att snygga till det lite

--- Up8QubitsReal.py
+++ Up8QubitsReal.py
@@ -50,7 +50,7 @@ inputs = [[np.pi, t_1q, 10*1e-9],   # [drive angle0, drive time 0, drive start 0
           [np.pi/5,t_1q, 0    ],
           [np.pi*2,t_1q, 0     ]]
 
-Qubitstates0 =np.zeros(NoOfQubits) #Initial state of qubits (all = 0), change as you please
+Qubitstates0 =np.zeros(NoOfQubits) #Initial state of qubits (all = 1), change as you please
 
 
 
@@ -72,116 +72,104 @@ for j in range(NoOfQubits-1):
     psi0 = tensor(psi0, basis(2,int(Qubitstates0[j+1])))
 
 a=destroy(2)
-# Create list of gates that could run on each qubit. That is gate[0] => operate on qubit1, gate[3] => operate on qubit4
-gate=[]
-gate.append(sigmax())
-gate.append(tensor(qeye(2), sigmax()))
-gate.append(tensor(qeye(2),qeye(2), sigmax()))
-gate.append(tensor(qeye(2),qeye(2),qeye(2), sigmax()))
-gate.append(tensor(qeye(2),qeye(2),qeye(2),qeye(2), sigmax()))
-gate.append(tensor(qeye(2),qeye(2),qeye(2),qeye(2),qeye(2), sigmax()))
-gate.append(tensor(qeye(2),qeye(2),qeye(2),qeye(2),qeye(2),qeye(2), sigmax()))
-gate.append(tensor(qeye(2),qeye(2),qeye(2),qeye(2),qeye(2),qeye(2),qeye(2), sigmax()))
-for i in range(NoOfQubits):
-    for j in range(NoOfQubits-1-i):
-        gate[i]=tensor(gate[i], qeye(2))
-del gate[NoOfQubits:]
 
+
+def DimensionifyOperator(Q, NoOfQubits, vals):
+    #Create list of operators that could act on each qubit. 
+    # That is gate[0] => operate on qubit1, gate[3] => operate on qubit4.. etc
+    # Q = Operator, vals= array of length MaxNoOQubits with values s.a. rates for each qubit
+    Opers = []
+    Opers.append(vals[0]*Q)   #Relaxation
+    Opers.append(tensor(qeye(2),vals[1]*Q))   #Relaxation
+    Opers.append(tensor(qeye(2),qeye(2),vals[2]*Q))   #Relaxation
+    Opers.append(tensor(qeye(2),qeye(2),qeye(2),vals[3]*Q) )  #Relaxation
+    Opers.append(tensor(qeye(2),qeye(2),qeye(2),qeye(2),vals[4]*Q))   #Relaxation
+    Opers.append(tensor(qeye(2),qeye(2),qeye(2),qeye(2),qeye(2),vals[5]*Q) )  #Relaxation
+    Opers.append(tensor(qeye(2),qeye(2),qeye(2),qeye(2),qeye(2),qeye(2),vals[6]*Q)  ) #Relaxation
+    Opers.append(tensor(qeye(2),qeye(2),qeye(2),qeye(2),qeye(2),qeye(2),qeye(2),vals[7]*Q) )  #Relaxation
+    
+    for i in range(NoOfQubits):
+        for j in range(NoOfQubits-1-i):
+            Opers[i]=tensor(Opers[i], qeye(2))
+    del Opers[NoOfQubits:]
+    
+    return Opers
+
+
+# Create list of gates that could run on each qubit. That is gate[0] => operate on qubit1, gate[3] => operate on qubit4
+gate=DimensionifyOperator(sigmax(), NoOfQubits, np.ones(MaxNoOfQubits))
 
 #%%Timefuncs
-def Et0 (t, args): # drive pulse E(t) = A*cos^2(tπ/t_qd - π/2),  
+def EnvelopeFunc (t, beta, t_qd, t_st):
+    # Models the drive pulse as E(t) = A*cos^2(tπ/t_qd - π/2), 
+    # Used in the time functions Et# which are used in H = QobjEvo 
+    A    = beta/t_qd
+    E = A*np.cos((t-t_st)*np.pi/t_qd-np.pi/2)**2
+    if t_st<t<t_qd+t_st:
+         return E
+    else: 
+        return 0
+
+def Et0 (t, args): 
     # args in form {'beta': value, 't_qd': value}
     beta = args['Et0'][0] #Drive angle 
     t_qd = args['Et0'][1] #Gate time 
     t_st = args['Et0'][2] #Start time for drive 
-    A    = beta/t_qd
-    E = A*np.cos((t-t_st)*np.pi/t_qd-np.pi/2)**2
-    if t_st<t<t_qd+t_st:
-         return E
-    else: 
-        return 0
-def Et1(t, args): # drive pulse E(t) = A*cos^2(tπ/t_qd - π/2),  
+    return EnvelopeFunc(t, beta, t_qd, t_st)
+
+def Et1(t, args): 
     # args in form {'beta': value, 't_qd': value}
     beta = args['Et1'][0] #Drive angle 
     t_qd = args['Et1'][1] #Gate time 
     t_st = args['Et1'][2] #Start time 
-    A    = beta/t_qd
-    E = A*np.cos((t-t_st)*np.pi/t_qd-np.pi/2)**2
-    if t_st<t<t_qd+t_st:
-         return E
-    else: 
-        return 0
-def Et2 (t, args): # drive pulse E(t) = A*cos^2(tπ/t_qd - π/2),  
+    return EnvelopeFunc(t, beta, t_qd, t_st)
+
+def Et2 (t, args): 
     # args in form {'beta': value, 't_qd': value}
     beta = args['Et2'][0] #Drive angle 
     t_qd = args['Et2'][1] #Gate time 
     t_st = args['Et2'][2] #Start time for drive 
-    A    = beta/t_qd
-    E = A*np.cos((t-t_st)*np.pi/t_qd-np.pi/2)**2
-    if t_st<t<t_qd+t_st:
-         return E
-    else: 
-        return 0
-def Et3(t, args): # drive pulse E(t) = A*cos^2(tπ/t_qd - π/2),  
+    return EnvelopeFunc(t, beta, t_qd, t_st)
+
+def Et3(t, args): 
     # args in form {'beta': value, 't_qd': value}
     beta = args['Et3'][0] #Drive angle 
     t_qd = args['Et3'][1] #Gate time 
     t_st = args['Et3'][2] #Start time 
-    A    = beta/t_qd
-    E = A*np.cos((t-t_st)*np.pi/t_qd-np.pi/2)**2
-    if t_st<t<t_qd+t_st:
-         return E
-    else: 
-        return 0
-def Et4 (t, args): # drive pulse E(t) = A*cos^2(tπ/t_qd - π/2),  
+    return EnvelopeFunc(t, beta, t_qd, t_st)
+
+def Et4 (t, args): 
     # args in form {'beta': value, 't_qd': value}
     beta = args['Et4'][0] #Drive angle 
     t_qd = args['Et4'][1] #Gate time 
     t_st = args['Et4'][2] #Start time for drive 
-    A    = beta/t_qd
-    E = A*np.cos((t-t_st)*np.pi/t_qd-np.pi/2)**2
-    if t_st<t<t_qd+t_st:
-         return E
-    else: 
-        return 0
-def Et5(t, args): # drive pulse E(t) = A*cos^2(tπ/t_qd - π/2),  
+    return EnvelopeFunc(t, beta, t_qd, t_st)
+
+def Et5(t, args): 
     # args in form {'beta': value, 't_qd': value}
     beta = args['Et5'][0] #Drive angle 
     t_qd = args['Et5'][1] #Gate time 
     t_st = args['Et5'][2] #Start time 
-    A    = beta/t_qd
-    E = A*np.cos((t-t_st)*np.pi/t_qd-np.pi/2)**2
-    if t_st<t<t_qd+t_st:
-         return E
-    else: 
-        return 0
-def Et6 (t, args): # drive pulse E(t) = A*cos^2(tπ/t_qd - π/2),  
+    return EnvelopeFunc(t, beta, t_qd, t_st)
+
+def Et6 (t, args):
     # args in form {'beta': value, 't_qd': value}
     beta = args['Et6'][0] #Drive angle 
     t_qd = args['Et6'][1] #Gate time 
     t_st = args['Et6'][2] #Start time for drive 
-    A    = beta/t_qd
-    E = A*np.cos((t-t_st)*np.pi/t_qd-np.pi/2)**2
-    if t_st<t<t_qd+t_st:
-         return E
-    else: 
-        return 0
-def Et7(t, args): # drive pulse E(t) = A*cos^2(tπ/t_qd - π/2),  
+    return EnvelopeFunc(t, beta, t_qd, t_st)
+
+def Et7(t, args): 
     # args in form {'beta': value, 't_qd': value}
     beta = args['Et7'][0] #Drive angle 
     t_qd = args['Et7'][1] #Gate time 
     t_st = args['Et7'][2] #Start time 
-    A    = beta/t_qd
-    E = A*np.cos((t-t_st)*np.pi/t_qd-np.pi/2)**2
-    if t_st<t<t_qd+t_st:
-         return E
-    else: 
-        return 0
+    return EnvelopeFunc(t, beta, t_qd, t_st)
 
 #%% Time dependance of H 
 inp={'Et0': inputs[0],'Et1': inputs[1],'Et2': inputs[2],'Et3': inputs[3],'Et4': inputs[4],'Et5': inputs[5],'Et6': inputs[6],'Et7': inputs[7]}
 
-# Function 
+# 
 if len(gate)==1:
     H = QobjEvo([gate[0],Et0],args=inp)
 elif len(gate)==2:
@@ -201,52 +189,17 @@ elif len(gate)==8:
     
 
 
-#%%
+#%% Expectations and collapse operators 
 
-e_ops=[]
-e_ops.append(a.dag()*a)
-e_ops.append(tensor(qeye(2), a.dag()*a))
-e_ops.append(tensor(qeye(2),qeye(2), a.dag()*a))
-e_ops.append(tensor(qeye(2),qeye(2),qeye(2), a.dag()*a))
-e_ops.append(tensor(qeye(2),qeye(2),qeye(2),qeye(2), a.dag()*a))
-e_ops.append(tensor(qeye(2),qeye(2),qeye(2),qeye(2),qeye(2), a.dag()*a))
-e_ops.append(tensor(qeye(2),qeye(2),qeye(2),qeye(2),qeye(2),qeye(2), a.dag()*a))
-e_ops.append(tensor(qeye(2),qeye(2),qeye(2),qeye(2),qeye(2),qeye(2),qeye(2), a.dag()*a))
-for i in range(NoOfQubits):
-    for j in range(NoOfQubits-1-i):
-        e_ops[i]=tensor(e_ops[i], qeye(2))
-del e_ops[NoOfQubits:]
+e_ops=DimensionifyOperator(a.dag()*a, NoOfQubits, np.ones(MaxNoOfQubits))
 
-c_ops1 = []
-c_ops2 = []
-rrate=np.sqrt(relax_rate)
-drate=np.sqrt(dephas_rate)
-c_ops1.append(rrate[0]*destroy(2))   #Relaxation
-c_ops2.append(drate[0]*(-sigmaz()/2))  #Dephasing 
-c_ops1.append(tensor(qeye(2),rrate[1]*destroy(2)))   #Relaxation
-c_ops2.append(tensor(qeye(2),drate[1]*(-sigmaz()/2)))  #Dephasing 
-c_ops1.append(tensor(qeye(2),qeye(2),rrate[2]*destroy(2)))   #Relaxation
-c_ops2.append(tensor(qeye(2),qeye(2),drate[2]*(-sigmaz()/2)))  #Dephasing 
-c_ops1.append(tensor(qeye(2),qeye(2),qeye(2),rrate[3]*destroy(2)) )  #Relaxation
-c_ops2.append(tensor(qeye(2),qeye(2),qeye(2),drate[3]*(-sigmaz()/2)) ) #Dephasing 
-c_ops1.append(tensor(qeye(2),qeye(2),qeye(2),qeye(2),rrate[4]*destroy(2)))   #Relaxation
-c_ops2.append(tensor(qeye(2),qeye(2),qeye(2),qeye(2),drate[4]*(-sigmaz()/2)))  #Dephasing 
-c_ops1.append(tensor(qeye(2),qeye(2),qeye(2),qeye(2),qeye(2),rrate[5]*destroy(2)) )  #Relaxation
-c_ops2.append(tensor(qeye(2),qeye(2),qeye(2),qeye(2),qeye(2),drate[5]*(-sigmaz()/2)) ) #Dephasing 
-c_ops1.append(tensor(qeye(2),qeye(2),qeye(2),qeye(2),qeye(2),qeye(2),rrate[6]*destroy(2))  ) #Relaxation
-c_ops2.append(tensor(qeye(2),qeye(2),qeye(2),qeye(2),qeye(2),qeye(2),drate[6]*(-sigmaz()/2)))  #Dephasing 
-c_ops1.append(tensor(qeye(2),qeye(2),qeye(2),qeye(2),qeye(2),qeye(2),qeye(2),rrate[7]*destroy(2)) )  #Relaxation
-c_ops2.append(tensor(qeye(2),qeye(2),qeye(2),qeye(2),qeye(2),qeye(2),qeye(2),drate[7]*(-sigmaz()/2)) ) #Dephasing 
-for i in range(NoOfQubits):
-    for j in range(NoOfQubits-1-i):
-        c_ops1[i]=tensor(c_ops1[i], qeye(2))
-        c_ops2[i]=tensor(c_ops2[i], qeye(2))
-del c_ops1[NoOfQubits:]
-del c_ops2[NoOfQubits:]
+c_ops1 = DimensionifyOperator(a.dag()*a, NoOfQubits, np.sqrt(relax_rate))
+c_ops2 = DimensionifyOperator(a.dag()*a, NoOfQubits, np.sqrt(dephas_rate))
 
 c_ops=c_ops1+c_ops2
 
-#%%
+
+#%% Results 
 
 if use_MCWF:
     result = mcsolve(H, psi0, tlist, c_ops, e_ops, ntraj)
