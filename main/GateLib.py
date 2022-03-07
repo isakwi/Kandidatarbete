d5565f8c7f0ba28ad96cdf5fd1c7754036a31cb4
isakwi <isakwi@student.chalmers.se>
2022-03-07 11:08:46 +0100

    Added some gates to GateLib

--- main/GateLib.py
+++ main/GateLib.py
@@ -1,7 +1,42 @@
 """
 Backend with definition of 1qb and 2qb gates
-
 """
+
+from qutip import *
+
+def PX(Qblist, target):
+    """Creates specific sigmax gate, maybe better than to create all gates? Then
+    you can use only the operators you need.
+    Input is list of qubits and which qubit you want to target with the operator"""
+
+    sx = [qeye(Qb.level) for Qb in Qblist]
+    sx[target] = destroy(Qblist[target].level) + create(Qblist[target].level)
+    return tensor(sx)
+
+def PY(Qblist, target):
+    """Creates specific sigmay gate, maybe better than to create all gates? Then
+    you can use only the operators you need.
+    Input is list of qubits and which qubit you want to target with the operator"""
+    sy = [qeye(Qb.level) for Qb in Qblist]
+    sy[target] = -1j * (destroy(Qblist[target].level) - create(Qblist[target].level))
+    return tensor(sy)
+
+def PM(Qblist, target):
+    """Creates specific sigma- gate, maybe better than to create all gates? Then
+    you can use only the operators you need.
+    Input is list of qubits and which qubit you want to target with the operator"""
+    sm = [qeye(Qb.level) for Qb in Qblist]
+    sm[target] = destroy(Qblist[target].level)
+    return tensor(sm)
+
+def PZ(Qblist, target):
+    """Creates specific sigmaz gate, maybe better than to create all gates? Then
+    you can use only the operators you need.
+    Input is list of qubits and which qubit you want to target with the operator
+    This might need to be changed to be a "virtual" gate """
+    sz = [qeye(Qb.level) for Qb in Qblist]
+    sz[target] = create(Qblist[target].level)*destroy(Qblist[target].level)
+    return tensor(sz)
 
 
 
--- main/main.py
+++ main/main.py
@@ -3,9 +3,10 @@
 import read_data as rd
 import Qb_class as Qb
 
-""" Eventually the number of qubits will be read from OpenQASM instead!"""
-# Parameters
+# Parameters, eventually the number of qubits and the levels will be read from OpenQASM instead!
 n, relax, depha, inter, therma, l = rd.read_data()  # Parameters
 Qblist = []
 for i in range(0, n):  # Creates list with all qubits
     Qblist.append(Qb.Qubit(l[i], [relax[i], depha[i], inter[i], therma[i]], [], []))
+
+
