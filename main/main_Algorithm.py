1341eb1ef7170a76821c555c73cfd95cd52e0e48
blomaxel <99745239+blomaxel@users.noreply.github.com>
2022-03-08 10:13:12 +0100

    Update main_Algorithm.py
    First draft of main algorithm

--- main/main_Algorithm.py
+++ main/main_Algorithm.py
@@ -1,41 +1,34 @@
 ##This program takes as input:
 # *steps which includes t_list(s) and gates 
 # *c_ops
+# *anharmonicity
 # * psi0_start ? (number of qubits qubits and energy levels are the important things, all qubits are assumed to be in ground state)
 # * envelope fcn
 ##This program calls a function which performs the #gate algorithm"
 ##THis program should return the final state
 from qutip import *
 import numpy as np
-#TODO: replace everything until the for loop and
-
-## This script doesn't need to explicitly know the number of qubits
-##Assume that we have a steps[] data set
-## Also assume that the gates are implemented iin the Hamiltonian
-a = destroy(2)
-pi = np.pi
-beta = 4*pi/2e-9
-inputfile = {"steps" : [{"gate": a.dag() * a}], "psi0": (basis(2,0)+basis(2,1))/np.sqrt(2), "c_ops" : [np.sqrt(0.1) * a], "tlists" : [np.linspace(0,2e-9,101)], "virtualgates" : [None], "e_ops" : [a.dag() * a]}
-psi0 = []
-steps = inputfile["steps"]
-psi0.append(inputfile["psi0"]) #initial state before the algorithm is run
-c_ops = inputfile["c_ops"]
-tlists = inputfile["tlists"]
-e_ops = inputfile["e_ops"]
-virtualgates = inputfile["virtualgates"] #rotations around the z axis
-
 
 
 ##--------EVERYTHING ABOVE THIS IS JUST TAKING IN INPUT
-for i in range(len(steps)):
-    #todo: Calla the gate algorithm to obtain the Hamiltonian for this step 
-    H = beta/2 * steps[i]["gate"]
-    output = mcsolve(H,psi0[i], tlists[i],  c_ops= c_ops, ntraj = 1)
-    currentstate = output.states
-    #the virtual gates should be able to apply through matrix multiplication
-    if virtualgates[i] != None:
-        currentstate = virtualgates[i] * currentstate
-    psi0.append(currentstate)
-    print(psi0[-1])
-    #TODO: Return the final state (we will need to "functionize" this code)
-
+def main_algorithm(args):
+    steps = args["steps"]
+    c_ops = args["c_ops"]
+    psi0 = args["psi0"]
+    if "ntraj" in args:
+        ntraj = args["ntraj"]
+    else:
+        ntraj = 500
+    H0 = ZZ_Interaction(Qblist) + anharmonicity(Qblist)
+    for i in range(len(steps)): #each 
+        gates = gate_algorithm(steps[i], Qblist) #gates contains "physical gates", virtual gates, t_list, IN THAT ORDER
+        H = gates[0] +  H0
+        virtualgate = gates[1]
+        tlist = gates[2]
+        output = mcsolve(H,psi0, tlist, c_ops = c_ops, ntraj = ntraj)
+        psi0 = output.states[-1]
+        #the virtual gates should be able to apply through matrix multiplication
+        if virtualgate != None:
+            psi0 = virtualgates[i] * psi0
+    return psi0
+    
