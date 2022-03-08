88717efb43bb2307f525641975ebf3c306dec12c
blomaxel <99745239+blomaxel@users.noreply.github.com>
2022-03-08 10:36:58 +0100

    Update CollapseOperator_function.py
    deleted an unnecessary for loop

--- main/CollapseOperator_function.py
+++ main/CollapseOperator_function.py
@@ -27,16 +27,6 @@ def Collapse_ops(qb_vec):
         if qb[1].noisert_vec[1] > 0.0: #creating dephasing collapse operator
             eye_vec[qb[0]] = destroy(qb[1].level) * destroy(qb[1].level).dag()
             cops_vec.append(np.sqrt(qb[1].noisert_vec[1]) * tensor(eye_vec))
-
-        for q in enumerate(qb_vec):
-
-
-            if 0 < np.sqrt((q[1].desig[0] - qb[1].desig[0])**2 + (q[1].desig[1] - qb[1].desig[1])**2) < 1.0 and qb[1].noisert_vec[2] :
-                eye_vec[qb[0]] = (destroy(qb[1].level).dag() * destroy(qb[1].level))
-                eye_vec2[q[0]] = (destroy(q[1].level).dag() * destroy(q[1].level))
-
-                cops_vec.append(np.sqrt((qb[1].noisert_vec[2] + q[1].noisert_vec[2])/2) * (tensor(eye_vec) * tensor(eye_vec2) + tensor(eye_vec2) * tensor(eye_vec)))
-
     return cops_vec
 
 
