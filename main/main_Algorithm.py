c2f8d2892c17e7c940419e4d7eeacbe0ea88c2de
isakwi <isakwi@student.chalmers.se>
2022-03-09 10:16:15 +0100

    Fixed parfor for the main algorithm

--- main/main_Algorithm.py
+++ main/main_Algorithm.py
@@ -9,6 +9,7 @@
 from qutip import *
 import numpy as np
 import GateFuncs as gf
+import mcsolving
 
 
 ##--------EVERYTHING ABOVE THIS IS JUST TAKING IN INPUT
@@ -23,7 +24,7 @@ def main_algorithm(args):
     else:
         ntraj = 500
 
-    H0 = 0  + anharmonicity(U, Qblist) # + ZZ_Interaction(Qblist)
+    H0 = 0  #+ anharmonicity(U, Qblist) # + ZZ_Interaction(Qblist)
 
     ## Do first iteration for ntraj trajectories to split the mcsolve
     gates = gf.CreateHfromStep(steps[0], Qblist)  # gates contains "physical gates", virtual gates, t_list, IN THAT ORDER
@@ -31,21 +32,16 @@ def main_algorithm(args):
     virtualgate = gates[1]
     tlist = gf.TimeDepend(steps[0], gates[0], gates[2])[1]
     output = mcsolve(H, psi0, tlist, c_ops=c_ops, ntraj=ntraj)
-    psi0 = output.states[:, -1]
+    psi0 = []
+    for idx in range(len(output.states[:,-1])):
+        psi0.append(output.states[idx,-1])
 
     for i in range(1,len(steps)): #each step except the first one
-        psilist = []
         gates = gf.CreateHfromStep(steps[i], Qblist)  # gates contains "physical gates", virtual gates, t_list, IN THAT ORDER
         H = gf.TimeDepend(steps[i], gates[0], gates[2])[0] + H0
         virtualgate = gates[1]
         tlist = gf.TimeDepend(steps[i], gates[0], gates[2])[1]
-        for psi in psi0:
-            output = mcsolve(H,psi, tlist, c_ops = c_ops, ntraj = 1, progress_bar=None)
-            outstate = (output.states[:,-1])
-            psilist.append(outstate[0])
-            # the virtual gates should be able to apply through matrix multiplication
-            # if virtualgate != None:
-            #   psi0 = virtualgates[i] * psi0
-        psi0 = psilist
+        psi_temp = parfor(mcsolving.mcs, psi0, H=H, tlist=tlist, c_ops=c_ops)
+        psi0 = psi_temp
     return psi0
     
