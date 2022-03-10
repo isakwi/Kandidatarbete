5708c3c3d7a68417be6d275a3e71290651c165c9
edvinmartinson <edvmar@student.chalmers.se>
2022-03-10 16:35:49 +0100

    Corrected HD after testing. (No 1/√2 was needed)

--- main/GateLib.py
+++ main/GateLib.py
@@ -73,8 +73,8 @@ def HD(Qblist, target):
     Returns two operations, one real and one virtual. The virtual is to be applied after the alg-step
     NOTE: Angle for HD_real is always pi/2 and for HD_virt always pi"""
     #HD = sqrtm(PY(Qblist, target)) * PZ(Qblist,target) #we don't know if sqrtm works
-    HD_real = 1/np.sqrt(2) * PY(Qblist, target)
-    HD_virt = 1/np.sqrt(2) * VPZ(Qblist, target, np.pi)
+    HD_real = PY(Qblist, target)
+    HD_virt = VPZ(Qblist, target, np.pi)
     return [HD_real, HD_virt]
 
 def CNOT(Qblist, Tar_Con):
