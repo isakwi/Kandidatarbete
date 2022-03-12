de3b9bcc4eaa46f5da9b3d47927f02937c0aa3c0
isakwi <isakwi@student.chalmers.se>
2022-03-12 14:02:14 +0100

    Added some comments because I think we will need to change this

--- main/ZZinteraction_function.py
+++ main/ZZinteraction_function.py
@@ -16,6 +16,9 @@ def ZZ_interaction(Qblist):
 
     for qb in enumerate(Qblist): #enumerates through array of Qubits and creates interaction operators qubits that interacts
         eyeqb = eye_vec.copy()
+        """Needs to change this since Anton said that interaction only will be given as a matrix
+        From what I understand we wont need to check the position and how close qubits are
+        if we have that matrix?"""
 
         if qb[1].noisert_vec[2] > 0.0: # checks to see if there is any noise and adds sigmaz for qubit
             eyeqb[qb[0]] = destroy(qb[1].level).dag() * destroy(qb[1].level)
