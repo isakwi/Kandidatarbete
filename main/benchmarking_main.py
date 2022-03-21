1eaa45cac55cf4cfb51631c50615e4f551260030
isakwi <isakwi@student.chalmers.se>
2022-03-21 11:35:43 +0100

    Saw now that Albin had done something similar, might use that together with this file or only one of them so will leave this one for now

--- main/benchmarking_main.py
+++ main/benchmarking_main.py
@@ -9,11 +9,15 @@ import GateFuncs as gf
 import main_Algorithm as mA
 pi = np.pi
 
+
 def benchmarking(Qblist):
     c_ops = co.create_c_ops(Qblist)
     psi0 = 1  # Some initial state
     n = 5000  # Number of iterations
     F = 1  # Some cost function
+    J = 1
+    gamma = 1
+    beta = 1
     for iteration in range(n):
         # Do some quantum algorithm
         # Do measurements
