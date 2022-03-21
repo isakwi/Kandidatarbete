1263617ef5220087729da132f7ec283080202ce2
diddep <didrikp@student.chalmers.se>
2022-03-21 19:20:56 +0100

    made some progress on the benchmark test

--- main/benchmarking_main.py
+++ main/benchmarking_main.py
@@ -19,6 +19,7 @@ def benchmarking(Qblist):
     gamma = 1
     beta = 1
     for iteration in range(n):
+        p=0 # tvungen att lägga in för att kunna köra (indentation)
         # Do some quantum algorithm
         # Do measurements
         # Optimize cost function
