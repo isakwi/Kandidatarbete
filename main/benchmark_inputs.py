c7b000ce4a1308824392ae720564e8ecb92f191e
blomaxel <99745239+blomaxel@users.noreply.github.com>
2022-03-21 11:48:35 +0100

    Added the quantum algorithm step input

--- main/benchmark_inputs.py
+++ main/benchmark_inputs.py
@@ -2,14 +2,26 @@ import numpy as np
 import GateFuncs as gf 
 
 gamma = [1,1]
-J = 1
+cangle = gamma[0] #cangle = gamma (thought the name was suitable since it comes with \hat{C}
+bangle = gamma[1] #cangle = beta (thought the name was suitable since it comes with \hat{B}
+J = 0.5
+h1, h2 = -0.5 , 0
 
 steps = []
+#First we apply Hadamard to both qubits
+steps.append(gf.Add_step(["HD","HD"], [0,1], [0,0]))
+steps.append(gf.Add_step(["HD"], [1], [0]))
+steps.append(gf.Add_step(["HD"], [1], [0]))
+steps.append(gf.Add_step(["PX"], [1], [2*cangle*J]))
+steps.append(gf.Add_step(["HD"], [1], [0]))
+steps.append(gf.Add_step(["PZ","PZ"], [0,1], [2*cangle*h1, 2*cangle*h2]))
+steps.append(gf.Add_step(["PX","PX"], [0,1], [2*bangle, 2*bangle]))
 
-steps.append(gf.Add_step(["HD"], [1], [2*gamma_i * J]))
-#steps.append(gf.Add_step(["CZ"]))
 
 """
 This is meant to be a file to easily implement the benchmark. I don't know if this is how 
 we want to do it, but then we can just remove the file // Albin
+
+I added one step (p = 1) of the gates as they are defined in the paper (PHYS. REV. APPLIED 14, 034010 (2020))
+I am unsure of how we define the angle for the Hadamard, I wrote 0 for now // Axel
 """
