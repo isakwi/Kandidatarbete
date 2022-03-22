ff07b113e1a9ded6414ad5ccb5f5530af9154306
isakwi <isakwi@student.chalmers.se>
2022-03-22 11:03:40 +0100

    Fixed steps and args

--- main/benchmark_inputs.py
+++ main/benchmark_inputs.py
@@ -2,7 +2,6 @@ import numpy as np
 import GateFuncs as gf
 import CollapseOperator_function as colf
 import main_Algorithm as ma
-import main as m
 from qutip import *
 import GateLib as gl
 import matplotlib.pyplot as plt
@@ -12,14 +11,12 @@ import Qb_class as qbc
 #qubits
 qb1 = qbc.Qubit(3, [0.01,0.01,0.01], 0.1, [1,1], [1,0,0])
 qb2 = qbc.Qubit(3, [0.01,0.01,0.01], 0.1, [2,2], [1,0,0])
-
 #list of angles for parameters
 gamma_vec = np.linspace(0, np.pi,20)
 qblist = [qb1, qb2]
 
 #zeros matrix for saving expectation value of hamiltonian
 exp_mat = np.zeros((220, 20))
-
 c_ops = colf.create_c_ops(qblist)
 #number of trajectories
 ntraj = 20
@@ -41,14 +38,14 @@ for i in range(0, 20):
         # First we apply Hadamard to both qubits
         steps.append(gf.Add_step(["HD", "HD"], [0, 1], [0, 0]))
         steps.append(gf.Add_step(["HD"], [1], [0]))
-        steps.append(gf.Add_step(["CZnew"], [[1, 0]]))
+        steps.append(gf.Add_step(["CZnew"], [[1, 0]], [0]))
         steps.append(gf.Add_step(["PX"], [1], [2 * cangle * J]))
-        steps.append(gf.Add_step(["CZnew"], [[1, 0]]))
+        steps.append(gf.Add_step(["CZnew"], [[1, 0]], [0]))
         steps.append(gf.Add_step(["HD"], [1], [0]))
         steps.append(gf.Add_step(["PZ", "PZ"], [0, 1], [2 * cangle * h1, 2 * cangle * h2]))
         steps.append(gf.Add_step(["PX", "PX"], [0, 1], [2 * bangle, 2 * bangle]))
 #calling main_algorithm
-        args = {"steps" : steps, "c_ops" : c_ops, "psi0" : psi0, "qblist": qblist, "tmax": tmax, "ntraj" : ntraj}
+        args = {"steps" : steps, "c_ops" : c_ops, "psi0" : psi0, "Qblist": qblist, "t_max": tmax, "ntraj" : ntraj}
 
         state = ma.main_algorithm(args)
 #saving mean value of expectation value in matrix
