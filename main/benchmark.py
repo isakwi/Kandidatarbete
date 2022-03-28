bf8dc501bb1aad1f566db5cf2dd4477bd6bb9714
isakwi <isakwi@student.chalmers.se>
2022-03-28 11:36:55 +0200

    Fixed so the benchmark can have different resolution on gamma and beta which KD said would be good to find errors

--- main/benchmark.py
+++ main/benchmark.py
@@ -16,14 +16,16 @@ c = 0.00
 qb1 = qbc.Qubit(3, [c, c, c], -229e6 * 2 * pi, [1,1], [1,0,0])
 qb2 = qbc.Qubit(3, [c, c, c], -225e6 * 2 * pi, [2,2], [1,0,0])
 
-resolution = 4
+gamma_resolution = 6
+beta_resolution = 4
 
 #list of angles for parameters
-gamma_vec = np.linspace(0, pi, resolution)
+gamma_vec = np.linspace(0, pi, gamma_resolution)
+beta_vec = np.linspace(0, pi, beta_resolution)
 qblist = [qb1, qb2]
 
 #zeros matrix for saving expectation value of hamiltonian
-exp_mat = np.zeros((resolution, resolution))
+exp_mat = np.zeros((beta_resolution, gamma_resolution))
 c_ops = colf.create_c_ops(qblist)
 #number of trajectories
 ntraj = 100
@@ -54,26 +56,27 @@ steps[5] = (gf.Add_step(["HD"], [1], [0]))
 
 #iterating through list of angles and saving expectationvalues in matrix
 t0 = time.time()
-for i in range(0, resolution):
-    cangle = gamma_vec[i]
+for i in range(0, gamma_resolution):
+    gamma = gamma_vec[i]
     if i > 0:
         t = time.time()
         print("Time elapsed: %.2f seconds." %(t-t0))
-        print("Estimated time left: %.2f seconds. \n" %((resolution-i) * (t-t0)/i))
-    for j in range(0, resolution):
-        bangle = gamma_vec[j]
-        steps[3] = (gf.Add_step(["PX"], [1], [2 * cangle * J]))
-        steps[6] = (gf.Add_step(["VPZ", "VPZ"], [0, 1], [2 * cangle * h1, 2 * cangle * h2]))
-        steps[7] = (gf.Add_step(["PX", "PX"], [0, 1], [2 * bangle, 2 * bangle]))
+        print("Estimated time left: %.2f seconds. \n" %((gamma_resolution-i) * (t-t0)/i))  # Change here
+    for j in range(0, beta_resolution):
+        beta = beta_vec[j]
+        steps[3] = (gf.Add_step(["PX"], [1], [2 * gamma * J]))
+        steps[6] = (gf.Add_step(["VPZ", "VPZ"], [0, 1], [2 * gamma * h1, 2 * gamma * h2]))
+        steps[7] = (gf.Add_step(["PX", "PX"], [0, 1], [2 * beta, 2 * beta]))
 #calling main_algorithm
         args = {"steps" : steps, "c_ops" : c_ops, "psi0" : psi0, "Qblist": qblist, "t_max": tmax, "ntraj" : ntraj}
         state = ma.main_algorithm(args)
 #saving mean value of expectation value in matrix
         exp_mat[j, i] = np.mean(expect(ham, state))  # Beta y-axis and gamma x-axis
 
-#plotting matrix, have to fix axis so it has angles
-#plt.matshow(exp_mat)  # We need to flip the matrix of we use the matshow !
-plt.contourf(gamma_vec, gamma_vec, exp_mat)  # This one plots the matrix with angles
+#plotting matrix
+#plt.matshow(exp_mat)  # We need to flip the matrix of we use the matshow
+#  Do this by putting exp_mat[beta_resolution-1-j, i] = np.mean(expect(ham, state)) in for loops!) !
+plt.contourf(gamma_vec, beta_vec, exp_mat)  # This one plots the matrix with angles
 plt.colorbar()
 plt.show()
 
@@ -87,8 +90,7 @@ for i in range(len(exp_mat)):
             min = exp_mat[i][j]
             coord = [i, j]
 print(f"Minimum value is {min} and matrix indices [{coord[0]}, {coord[1]}]")
-print(f"It is located at gamma = {gamma_vec[coord[1]]} and beta at {gamma_vec[coord[0]]}")
-
+print(f"It is located at gamma = {gamma_vec[coord[1]]} and beta at {beta_vec[coord[0]]}")
 
 """
 gamma = [1,1]
