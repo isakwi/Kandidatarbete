9886710da9ec9335d7c711ca86de9fc1f2556e23
blomaxel <bloaxel@student.chalmers.se>
2022-03-30 14:51:27 +0200

    implemented a reverse pauli y gate

--- main/benchmark.py
+++ main/benchmark.py
@@ -8,7 +8,6 @@ import time
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import Qb_class as qbc
-import sys
 import matplotlib as mpl
 pi = np.pi
 
@@ -18,8 +17,9 @@ c = 0.00
 qb1 = qbc.Qubit(3, [c, c, c], -229e6 * 2 * pi, [1,1], [1,0,0])
 qb2 = qbc.Qubit(3, [c, c, c], -225e6 * 2 * pi, [2,2], [1,0,0])
 
-gamma_resolution = 10
-beta_resolution = 11
+
+gamma_resolution = 4
+beta_resolution = 6
 
 # list of angles for parameters
 gamma_vec = np.linspace(0, pi, gamma_resolution)
@@ -32,8 +32,9 @@ c_ops = colf.create_c_ops(qblist)
 # number of trajectories
 ntraj = 100
 tmax= [50e-9, 271e-9]
-psi0 = qbc.create_psi0(qblist, 0)  # 0 is the groundtstae
-problem = 'e'
+psi0 = qbc.create_psi0(qblist, 0)  # 0 is the groundtstate
+problem = 'b'
+
 if problem == 'a':
     J, h1, h2 = 1/2, -1/2, 0
 elif problem == 'b':
@@ -42,18 +43,16 @@ elif problem == 'c':
     J, h1, h2 = 0, -1/2, -1/2
 elif problem == 'd':
     J, h1, h2 = 1, 0, 0
-else:
-    raise ValueError("You must do problem \'a\', \'b\', \'c\' or \'d\'")
 
 # Ising hHamiltonian, our cost function is the expectation value of this hamiltonian
 ham = h1 * gl.PZ(qblist, 0) + h2 * gl.PZ(qblist, 1) + J * gl.PZ(qblist, 0) * gl.PZ(qblist, 1)  # Maybe plus/minus
+
 # Changed the sign of J again and then it kinda worked
 
 # steps in algoritm (the ones that change will be updated for each step)
 steps = [gf.Add_step(["PX"],[0],[0.1]) for i in range(8)]  # zero angle rotation, will all be replaced
-
 steps[0] = (gf.Add_step(["HD", "HD"], [0, 1], [0, 0]))  # First we apply Hadamard to both qubits
-steps[1] = (gf.Add_step(["HD"], [1], [0]))
+steps[1] = (gf.Add_step([ "HD"], [ 1], [0]))  # Then we apply Hadamard to the second qubit
 steps[2] = (gf.Add_step(["CZnew"], [[1,0]], [2*pi]))
 steps[4] = (gf.Add_step(["CZnew"], [[1,0]], [2*pi]))
 steps[5] = (gf.Add_step(["HD"], [1], [0]))
@@ -76,13 +75,18 @@ for i in range(0, gamma_resolution):
 # calling main_algorithm
         args = {"steps" : steps, "c_ops" : c_ops, "psi0" : psi0, "Qblist": qblist, "t_max": tmax, "ntraj" : ntraj}
         state = ma.main_algorithm(args)
+        print(state)
 # saving mean value of expectation value in matrix
         exp_mat[j, i] = np.mean(expect(ham, state))  # Beta y-axis and gamma x-axis
 
 # plotting matrix
 # plt.matshow(exp_mat, cmap = plt.get_cmap('PiYG'))  # We need to flip the matrix of we use the matshow
 # Do this by putting exp_mat[beta_resolution-1-j, i] = np.mean(expect(ham, state)) in for loops!) !
+
+plt.contourf(gamma_vec, beta_vec, exp_mat, cmap = plt.get_cmap("PiYG"))  # This one plots the matrix with angles
+
 plt.contourf(gamma_vec, beta_vec, exp_mat, cmap = plt.get_cmap('PiYG'))  # This one plots the matrix with angles
+
 plt.colorbar()
 plt.show()
 
