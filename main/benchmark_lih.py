51da4b14e872284f1cc9e956f61e014af3e6ce29
diddep <didrikp@student.chalmers.se>
2022-04-11 14:04:20 +0200

    working on benchmark for LiH

--- main/benchmark_lih.py
+++ main/benchmark_lih.py
@@ -12,6 +12,7 @@ import Qb_class as qbc
 import matplotlib as mpl
 pi = np.pi
 tstart = time.time()
+steps=[]
 
 
 c = 0.01
@@ -22,3 +23,7 @@ qb1 = qbc.Qubit(3, [c, c, c], -225e6 * 2 * pi, [2,2], [1,0,0])
 qb2 = qbc.Qubit(3, [c, c, c], -229e6 * 2 * pi, [1,1], [1,0,0])
 qb3 = qbc.Qubit(3, [c, c, c], -225e6 * 2 * pi, [2,2], [1,0,0])
 
+def circuit(theta_arr):
+    steps.append(gf.Add_step(["PY", "PY", "PY", "PY"], [0, 1, 2, 3], [theta_arr[0],theta_arr[1],theta_arr[2],theta_arr[3],]))
+    steps.append(gf.Add_step(["CZnew"], [[1,0]], [0]))
+    steps.append(gf.Add_step(["PY", "CZnew"], [[0], [3,1]] ,[theta_arr[4], 0]))
