c71c9fed3511338ed2df87e500d6d1628bdcb5aa
isakwi <isakwi@student.chalmers.se>
2022-04-13 15:53:01 +0200

    Small fixes and comments

--- package_test/test.py
+++ package_test/test.py
@@ -1,3 +1,5 @@
 from qnas import *
 import qutip as qt
 help()
+# Some error with not being able to get ''help'' with finishing code lines, like when i start write hel...
+# I want pycharm to say help() in a list to the right, hmmm
