cf1652047eedef07022195d690dadbd77aa88c99
isakwi <isakwi@student.chalmers.se>
2022-05-03 16:24:44 +0200

    Added a Lot of error handling in solve, fixed some error and added documentation in help

--- qnas/src/qnas/solve.py
+++ qnas/src/qnas/solve.py
@@ -22,13 +22,13 @@ def solve(Qbfile = None, circuit = None, zz_int = None, ntraj=500, tmax=None, st
     if e_ops is None:
         e_ops = []
     if circuit is None:
-        print("You didn't enter an circuit circuit. QnAS will now exit?\t")
+        print("You didn't enter an circuit circuit. QnAS will now exit\t")
         return
 
     try:
         steps = opq.qasmToQnas(circuit)
     except:
-        print(f"Couldn't read the circuit file! Check that the circuit, {circuit}, is correct and that the circuit is"
+        print(f"Couldn't read the circuit file! Check that the circuit, {circuit}, is correct and that the circuit is "
               "constructed correctly. \nQnAS.solve() will now exit")
         return
 
@@ -54,33 +54,45 @@ def solve(Qbfile = None, circuit = None, zz_int = None, ntraj=500, tmax=None, st
         try:
             relax, depha, therma, anharm, levels = rd.readFile(Qbfile, n)
             Qblist = []
+            for i in range(0, n):  # Creates list with all qubits
+                anharm[i] = 2 * pi * anharm[i] * 1e6  # Convert linear frequency to angular (input seems to usually be linear)
+                relax[i] = 1e6 * relax[i]
+                depha[i] = 1e6 * depha[i]
+                therma[i] = 1e6 * therma[i]
+                Qblist.append(qbc.Qubit(levels[i], [relax[i], depha[i], therma[i]], anharm[i]))
         except:
-            print(f"Couldn't find file {Qbfile}. QnAS.solve() will now exit")
+            print(f"Couldn't read file {Qbfile}. Check name and data! QnAS.solve() will now exit")
             return
-        for i in range(0, n):  # Creates list with all qubits
-            anharm[i] = 2 * pi * anharm[i] * 1e6  # Convert linear frequency to angular (input seems to usually be linear)
-            relax[i] = 1e6*relax[i]
-            depha[i] = 1e6*depha[i]
-            therma[i] = 1e6*therma[i]
-            Qblist.append(qbc.Qubit(levels[i], [relax[i], depha[i], therma[i]], anharm[i]))
 
-    if not 1 <= ntraj <= 100000:
-        print("Invalid value for ntraj! Must be a positive integer with an upper limit of 100000 (to not kill your"
-              " computer). If you leave it unspecified, ntraj will automatically be 500. QnAS.solve() will now exit")
+
+    try:
+        if not 1 <= ntraj <= 100000:
+            print("Invalid value for ntraj! Must be a positive integer with an upper limit of 100000 (to not kill your"
+                  " computer). \n"
+                  "If you leave it unspecified, ntraj will automatically be 500. \n"
+                  "QnAS.solve() will now exit")
+            return
+    except:
+        print("Value of ntraj could not be decided! \n"
+              "QnAS.solve() will now exit")
         return
 
     if tmax is None:
         print("No input for tmax! QnAS will use 1qb gate time: 20ns and 2qb gate time: 200ns!")
         tmax = [20e-9, 200e-9]
 
-    if not (type(tmax) == list and len(tmax) == 2):
-        print("Invalid input for tmax! tmax must be a list of the form [1qb, 2qb] with "
-              "max gate times for 1-qubit-gates and 2-qubit-gates given in seconds!"
-              " QnAS.solve() will now exit")
-        return
-    if not (1e-12 < tmax[0] < 1e12 or 1e-12 < tmax[1] < 1e12):
-        print("Invalid values for at least one of the max gate times. Limits are 1e-12 < t < 1e12. "
-              " QnAS.solve() will now exit")
+    try:
+        if not (type(tmax) == list and len(tmax) == 2):
+            print("Invalid input for tmax! tmax must be a list of the form [1qb, 2qb] with "
+                  "max gate times for 1-qubit-gates and 2-qubit-gates given in seconds!\n"
+                  "QnAS.solve() will now exit")
+            return
+        if not (1e-12 < tmax[0] < 1e12 or 1e-12 < tmax[1] < 1e12):
+            print("Invalid values for at least one of the max gate times. Limits are 1e-12 < t < 1e12. \n"
+                  " QnAS.solve() will now exit")
+            return
+    except:
+        print("tmax could noy be decided. QnAS.solve() will now exit")
         return
     if type(store_time_dynamics) != bool:
         print("store_time_dynamics must be a boolean (True/False). "
@@ -90,21 +102,35 @@ def solve(Qbfile = None, circuit = None, zz_int = None, ntraj=500, tmax=None, st
         if e_ops is None:
             print("You didn't enter any e_ops, no need to save time dynamics!")
             store_time_dynamics = False
-        if type(e_ops) != list:
-            print("Wrong input type of e_ops! Input given on the form:\n"
-                  "e_ops_inp = [[e_op1, Tar_Con],[e_op2, Tar_Con], ... ]\n"
-                  "where e_op is a Qobj with the dimensions  ( qubit.level x qubit.level )\n"
-                  "and Tar_Con is the target and control in case of 2qb gate, as before.\n"
-                  "QnAS.solve() will now exit")
-            return
-        for e_op in e_ops:
-            if type(e_op) != list:
+
+        try:
+            if type(e_ops) != list:
                 print("Wrong input type of e_ops! Input given on the form:\n"
                       "e_ops_inp = [[e_op1, Tar_Con],[e_op2, Tar_Con], ... ]\n"
                       "where e_op is a Qobj with the dimensions  ( qubit.level x qubit.level )\n"
                       "and Tar_Con is the target and control in case of 2qb gate, as before.\n"
                       "QnAS.solve() will now exit")
                 return
+            for e_op in e_ops:
+                if type(e_op) != list:
+                    print("Wrong input type of e_ops! Input given on the form:\n"
+                          "e_ops_inp = [[e_op1, Tar_Con],[e_op2, Tar_Con], ... ]\n"
+                          "where e_op is a Qobj with the dimensions  ( qubit.level x qubit.level )\n"
+                          "and Tar_Con is the target and control in case of 2qb gate, as before.\n"
+                          "QnAS.solve() will now exit")
+                    return
+                if not 0 <= e_op[1] <= n-1:
+                    print("Your e_ops has targeted a qubit out of the scope!\n"
+                          "QnAS.solve() will now exit")
+                    return
+                if not (e_op[0].shape[0] == Qblist[e_op[1]].level and e_op[0].shape[1] == Qblist[e_op[1]].level):
+                    print("Size of expectation operator doesn't match the levels for the qubit!")
+                    return
+
+        except:
+            print("Expectation operator could not be decided!\n"
+                  "QnAS.solve() will now exit")
+            return
             #if type(e_op[0]) != Qobj:  # Would like to add something like this but don't know how
 
     if zz_int is not None:
@@ -117,13 +143,29 @@ def solve(Qbfile = None, circuit = None, zz_int = None, ntraj=500, tmax=None, st
                   "QnAS.solve() will now exit")
             return
         for row in zz_int:
+            if type(row) != list:
+                print("Wrong format for the interaction matrix! Should be given as [[x1, x2, ... , xn], [...]]"
+                      "\nfor n qubits! QnAS.solve() will now exit")
+                return
             if not n <= len(row) <= 15:
                 print("The matrix can't be smaller than the number of qubits (or larger than 15)!\n "
                       "QnAS.solve() will now exit")
                 return
+            if not len(row) == len(zz_int) == n:
+                print("Warning! The interaction matrix size is not n x n!")
+    try:
+        psi0 = qbc.createPsi0(Qblist, 0)
+    except:
+        print("Initial state could not be decided!\n"
+              "QnAS.solve() will now exit")
+        return
+    try:
+        c_ops = co.createCollapseOperators(Qblist)
+    except:
+        print("Collapse operators could not be decided!\n"
+              "QnAS.solve() will now exit")
+        return
 
-    psi0 = qbc.createPsi0(Qblist, 0)
-    c_ops = co.createCollapseOperators(Qblist)
 
     if zz_int is None:
         args = {"steps" : steps, "c_ops" : c_ops, "psi0" : psi0, "Qblist": Qblist, "t_max": tmax, "ntraj" : ntraj,
