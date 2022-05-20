eb89b9e7caa423268444c82457a83eac180b24fb
edvmar <edvmar@student.chalmers.se>
2022-05-20 10:07:04 +0200

    Labels and better plots

--- Demo/Demo.py
+++ Demo/Demo.py
@@ -20,8 +20,11 @@ e_ops = [[qt.create(3)*qt.destroy(3), 0], [qt.create(3)*qt.destroy(3), 1], [k02,
 finalStates, expvals, tlist = qnas.solve(Qbfile='Demo/qubitData.csv', circuit=circuit, storeTimeDynamics=True, e_ops=e_ops)
 
 
-plt.plot(tlist, expvals[0], label='kvantbit 1')
-plt.plot(tlist, expvals[1], label='kvantbit 2')
-plt.plot(tlist, expvals[2], label=r'$P_{|02\rangle}$')
-plt.legend()
+plt.plot(tlist*1e9, expvals[0], label='kvantbit 1')
+plt.plot(tlist*1e9, expvals[1], label='kvantbit 2')
+plt.plot(tlist*1e9, expvals[2], label=r'$P_{|02\rangle}$')
+plt.plot([0, tlist[-1]*1e9], [1, 1], '--k', linewidth=1)
+plt.xlabel('t [ns]')
+plt.ylabel('Tillstånd')
+plt.legend(loc='lower right')
 plt.show()
