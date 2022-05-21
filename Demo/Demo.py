9693f3eafb3aeefeae61899b90cae13dead1d6f0
edvmar <edvmar@student.chalmers.se>
2022-05-21 16:03:42 +0200

    NIcer demo plots

--- Demo/Demo.py
+++ Demo/Demo.py
@@ -19,12 +19,13 @@ e_ops = [[qt.create(3)*qt.destroy(3), 0], [qt.create(3)*qt.destroy(3), 1], [k02,
 
 finalStates, expvals, tlist = qnas.solve(Qbfile='Demo/qubitData.csv', circuit=circuit, storeTimeDynamics=True, e_ops=e_ops)
 
-
-plt.plot(tlist*1e9, expvals[0], label='kvantbit 1')
-plt.plot(tlist*1e9, expvals[1], label='kvantbit 2')
-plt.plot(tlist*1e9, expvals[2], label=r'$P_{|02\rangle}$')
-plt.plot([0, tlist[-1]*1e9], [1, 1], '--k', linewidth=1)
+# Plotta resultatet
+plt.figure(figsize=(11.52,8.64))
+plt.plot(tlist*1e9, expvals[0], label='kvantbit 1',linewidth=3)
+plt.plot(tlist*1e9, expvals[1], label='kvantbit 2',linewidth=3)
+plt.plot(tlist*1e9, expvals[2], label=r'$P_{|02\rangle}$',linewidth=3)
+plt.plot([0, tlist[-1]*1e9], [1, 1], '--k', linewidth=1.5)
 plt.xlabel('t [ns]')
 plt.ylabel('Tillstånd')
-plt.legend(loc='lower right')
+plt.legend(loc='lower right',prop={'size': 18})
 plt.show()
