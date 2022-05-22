caa1bb07e5756bf55fd12ff71aaf5f69fc1e2464
edvmar <edvmar@student.chalmers.se>
2022-05-22 12:18:53 +0200

    NIcer demo plots

--- Demo/Demo.py
+++ Demo/Demo.py
@@ -8,9 +8,9 @@ import qnas
 # qnas.help()
 
 circuit = qiskit.QuantumCircuit(2)
-circuit.rx(pi/2,0)
-circuit.ry(pi,1)
-circuit.cz(0,1)
+circuit.rx(pi/2, 0)
+circuit.ry(pi, 1)
+circuit.cz(0, 1)
 
 print(circuit.draw())
 
@@ -20,7 +20,7 @@ e_ops = [[qt.create(3)*qt.destroy(3), 0], [qt.create(3)*qt.destroy(3), 1], [k02,
 finalStates, expvals, tlist = qnas.solve(Qbfile='Demo/qubitData.csv', circuit=circuit, storeTimeDynamics=True, e_ops=e_ops)
 
 # Plotta resultatet
-plt.figure(figsize=(11.52,8.64))
+plt.figure(figsize=(9.6, 7.2))
 plt.plot(tlist*1e9, expvals[0], label='kvantbit 1',linewidth=3)
 plt.plot(tlist*1e9, expvals[1], label='kvantbit 2',linewidth=3)
 plt.plot(tlist*1e9, expvals[2], label=r'$P_{|02\rangle}$',linewidth=3)
